62b28f6f77638ba628648ca62edf247c387da97c
Oscar Beaumont <oscar@otbeaumont.me>
2024-01-17 12:18:22 +0800

    HTML minification + bundle analyser

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
 import devServer from "@hono/vite-dev-server";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { visualizer } from "rollup-plugin-visualizer";
 
 process.env = { ...process.env, ...loadEnv("production", process.cwd(), "") };
 
 export default defineConfig({
   plugins: [
     solid(),
+    createHtmlPlugin({
+      minify: true,
+    }),
     devServer({
       entry: "./api/[[...route]].ts",
       shouldServeWithHono: (path) => path.startsWith("/api"),
     }),
+    !("VERCEL" in process.env) && visualizer(),
   ],
 });
